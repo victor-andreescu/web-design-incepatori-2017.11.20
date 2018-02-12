9d595679adec5d600d8b73dd4dd46e8fb8fd7d11
Victor Andreescu <victor.andreescu@gmail.com>
2018-02-12 21:54:42 +0200

    avansati sedinta 07

--- js/script.js
+++ js/script.js
@@ -49,3 +49,56 @@ $('.jsContactClose, .jsContact').on('click', function(event) {
 $('.jsContact .modal').on('click', function(event){
     event.stopPropagation();
 });
+
+
+$('.jsMainMenu a').on('click', function(event){
+    event.preventDefault();
+
+    var id = $(this).attr('href');
+
+    var headerHeight = $('.jsHeader').outerHeight();
+
+    var toGo = $(id).offset().top - headerHeight;
+
+    // $('body, html').scrollTop(toGo);
+
+    $('body, html').animate({
+        "scrollTop": toGo
+    }, 500);
+
+    console.log(  );
+});
+
+
+$('.jsTagMenu a').on('click', function(event){
+    event.preventDefault();
+
+    var selectedTag = $(this).data('tag');
+
+    if (selectedTag) {
+        // console.log( $('.jsPortfolioGallery > div').not('[data-tag="'+selectedTag+'"]') );
+        // $('.jsPortfolioGallery > div').show().not('[data-tag="'+selectedTag+'"]').hide();
+        $('.jsPortfolioGallery > div').each(function() {
+            var itemTag = $(this).data('tag');
+            if(itemTag && itemTag.indexOf(selectedTag) >= 0 ) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+
+    } else {
+        $('.jsPortfolioGallery > div').show();
+    }
+});
+
+$('.jsSlider').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    infinite: false,
+    nextArrow: '<button type="button" class="slick-next"><i class="fa fa-arrow-right"></i></button>',
+    prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-arrow-left"></i></button>'
+});
+
+
+$('.jsDatepicker').flatpickr();
